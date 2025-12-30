e6961a971129ae1a70fd23a11501246cc8bafb1e
iseungsang01 <lss010330@snu.ac.kr>
2025-12-30 15:42:25 +0900

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -2,9 +2,15 @@ import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL
 const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY
+const AdminPassWord = process.env.REACT_APP_ADMIN_PASSWORD // SQL의 admin_secret과 일치해야 함
 
-const AdminPassWord = process.env.REACT_APP_ADMIN_PASSWORD
+// 헤더에 비밀번호를 기본으로 포함하는 클라이언트 생성
+const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+  global: {
+    headers: {
+      'x-admin-secret': AdminPassWord,
+    },
+  },
+})
 
-const supabase = createClient(supabaseUrl, supabaseAnonKey)
-
-export {supabase, AdminPassWord}
+export { supabase, AdminPassWord }
